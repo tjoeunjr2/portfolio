13731b2c98cc74f1e94b8ced50436d2af03c6295
tjoeunjr2 <tjoeun.jr2@gmail.com>
2026-04-20 20:16:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-app = Flask(__name__, template_folder="templates", static_folder="static")
+app = Flask(
+    __name__,
+    template_folder=os.path.join(BASE_DIR, "templates"),
+    static_folder=os.path.join(BASE_DIR, "static")
+)
 
 projects = [
     {
